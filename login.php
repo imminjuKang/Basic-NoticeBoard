0fa0f83b29cd8ee97f7c3abf27f41fcd0d674ddb
강민주 <162291384+imminjuKang@users.noreply.github.com>
2025-06-09 13:32:30 +0900

    Update login.php
    prepared statement

--- login.php
+++ login.php
@@ -14,23 +14,24 @@ if (!$user_email || !$user_pwd){
 }
 
 // 아이디와 비밀번호 체크
-$sql = "SELECT * FROM user_list WHERE user_email = '$user_email'";
-$result = mysqli_query($connect, $sql);
-// 결과값 한 줄을 연관 배열로 바꿈 (assoc array) -> column 이름으로 값 접근 가능
+$sql = "SELECT * FROM user_list WHERE user_email = ?";
+$stmt = mysqli_prepare($connect, $sql);
+mysqli_stmt_bind_param($stmt, "s", $user_email);
+mysqli_stmt_execute($stmt);
+$result = mysqli_stmt_get_result($stmt);
 $user = mysqli_fetch_assoc($result);
 
-
-if ($user_email != $user['user_email'] || $user_pwd != $user['user_pwd'])
-{
-    echo "<script>alert('잘못된 아이디 혹은 비밀번호입니다');</script>";
+// register에서 password_hash()로 저장했기 때문에
+// password_verify()로 비밀번호 비교
+if ($user && password_verify($user_pwd, $user['user_pwd'])) {
+    $_SESSION['user_email'] = $user['user_email'];
+    echo "<script>alert('로그인 되었습니다');</script>";
     echo "<script>location.replace('./index.php');</script>";
     exit;
 }
-
-$_SESSION['user_email'] = $user_email;
-
-if (isset($_SESSION['user_email'])) {
-    echo "<script>alert('로그인 되었습니다');</script>";
+else
+{
+    echo "<script>alert('잘못된 아이디 혹은 비밀번호입니다');</script>";
     echo "<script>location.replace('./index.php');</script>";
     exit;
 }
